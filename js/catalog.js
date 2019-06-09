dc0c2032378d7027410db2b394095521f6cb8aea
Egrik <egor-office-mail@yandex.ru>
2019-06-09 23:30:58 +0300

    Правки

--- js/catalog.js
+++ js/catalog.js
@@ -1,14 +1,23 @@
-	   		var products = document.querySelectorAll(".product-btn-buy");
+	   		var productsBuy = document.querySelectorAll(".product-btn-buy");
+	   		var productsBookmark = document.querySelectorAll(".product-btn-bookmarks");
 	    	var cartPopup = document.querySelector(".modal-cart");
+	    	var bookmarkPopup = document.querySelector(".modal-bookmark");
+	    	var headerCart = document.querySelector(".header-btn-cart");
+	    	var headerBookmark = document.querySelector(".header-btn-bookmarks");
 	    	var cartClose = cartPopup.querySelector(".modal-close");
+	    	var bookmarkClose = bookmarkPopup.querySelector(".modal-close");
 	    	var overlay = document.querySelector(".overlay");
-	    	var continueShopping = document.querySelector(".btn-next");
+	    	var contShoppingCart = cartPopup.querySelector(".btn-next");
+	    	var contShoppingBook = bookmarkPopup.querySelector(".btn-next");
 
-	    	for (var i = 0; i <= products.length-1; i++) {
-	    		var product = products[i];
+	    	// --- КОРЗИНА ---
+
+	    	for (var i = 0; i <= productsBuy.length-1; i++) {
+	    		var product = productsBuy[i];
 	    		product.addEventListener("click", function(evt) {
 	    		evt.preventDefault();
 	    		cartPopup.classList.add("modal-show");
+	    		headerCart.classList.add("btn-users-add");
 	    		overlay.classList.add("modal-show-overlay");
 	    	});
 	    	}
@@ -25,7 +34,7 @@
 	    		overlay.classList.remove("modal-show-overlay");
 	    	}); 
 
-	    	continueShopping.addEventListener("click", function(evt) {
+	    	contShoppingCart.addEventListener("click", function(evt) {
 	    		evt.preventDefault();	    		
 	    		cartPopup.classList.remove("modal-show");
 	    		overlay.classList.remove("modal-show-overlay");
@@ -40,3 +49,46 @@
       			}
     		}
   			});
+
+  			// --- ЗАКЛАДКИ ---
+
+	    	for (var i = 0; i <= productsBookmark.length-1; i++) {
+	    		var productBook = productsBookmark[i];
+	    		productBook.addEventListener("click", function(evt) {
+	    		evt.preventDefault();
+	    		bookmarkPopup.classList.add("modal-show");
+	    		headerBookmark.classList.add("btn-users-add");
+	    		overlay.classList.add("modal-show-overlay");
+	    	});
+	    	}
+
+ 	    	bookmarkClose.addEventListener("click", function(evt) {
+	    		evt.preventDefault();	    		
+	    		bookmarkPopup.classList.remove("modal-show");
+	    		overlay.classList.remove("modal-show-overlay");
+	    	});
+
+	    	overlay.addEventListener("click", function(evt) {
+	    		evt.preventDefault();	    		
+	    		bookmarkPopup.classList.remove("modal-show");
+	    		overlay.classList.remove("modal-show-overlay");
+	    	}); 
+
+	    	contShoppingBook.addEventListener("click", function(evt) {
+	    		evt.preventDefault();	    		
+	    		bookmarkPopup.classList.remove("modal-show");
+	    		overlay.classList.remove("modal-show-overlay");
+	    	}); 
+
+	    	window.addEventListener("keydown", function (evt) {
+    			if (evt.keyCode === 27) {
+      				evt.preventDefault();
+      			if (bookmarkPopup.classList.contains("modal-show")) {
+        			bookmarkPopup.classList.remove("modal-show");
+        			overlay.classList.remove("modal-show-overlay");
+      			}
+    		}
+  			});
+
+
+
